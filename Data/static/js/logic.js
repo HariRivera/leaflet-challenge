585d5f2ebb6f9c9e7b610ecd5726c5cb34b87ec7
HariRivera <haririvera2010@gmail.com>
2024-06-10 21:40:47 -0600

    Adjustment

--- Data/static/js/logic.js
+++ Data/static/js/logic.js
@@ -1,15 +1,15 @@
-// URL of the earthquake data
+// URL of the earthquake dataset
 const url = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson";
 
-// Map creation
+// Create the map centered on an initial location
 const map = L.map('map').setView([20, 0], 2);
 
-// Add the base map
+// Add the base layer to the map
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 18,
 }).addTo(map);
 
-// Color based on th Depth
+// Function to determine color based on depth
 function getColor(depth) {
     return depth > 50 ? 'red' :
            depth > 30 ? 'orange' :
@@ -17,28 +17,26 @@ function getColor(depth) {
                         'green';
 }
 
-// Earthquake data gathering
-fetch(url)
-    .then(response => response.json())
-    .then(data => {
-        data.features.forEach(feature => {
-            const coords = feature.geometry.coordinates;
-            const properties = feature.properties;
-            const magnitude = properties.mag;
-            const depth = coords[2];
-            const location = [coords[1], coords[0]];
+// Fetch and process earthquake data using D3
+d3.json(url).then(data => {
+    data.features.forEach(feature => {
+        const coords = feature.geometry.coordinates;
+        const properties = feature.properties;
+        const magnitude = properties.mag;
+        const depth = coords[2];
+        const location = [coords[1], coords[0]];
 
-            // Map Marker
-            L.circleMarker(location, {
-                radius: magnitude * 2, 
-                color: getColor(depth),
-                fillColor: getColor(depth),
-                fillOpacity: 0.7
-            }).bindPopup(`<b>Magnitude:</b> ${magnitude}<br><b>Location:</b> ${properties.place}<br><b>Depth:</b> ${depth} km`).addTo(map);
-        });
+        // Add marker to the map
+        L.circleMarker(location, {
+            radius: magnitude * 2, // Adjust size
+            color: getColor(depth),
+            fillColor: getColor(depth),
+            fillOpacity: 0.7
+        }).bindPopup(`<b>Magnitude:</b> ${magnitude}<br><b>Location:</b> ${properties.place}<br><b>Depth:</b> ${depth} km`).addTo(map);
     });
+});
 
-// Crear la leyenda
+// Create the legend
 const legend = L.control({ position: 'bottomright' });
 
 legend.onAdd = function (map) {
